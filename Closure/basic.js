ef1e9957d3929d60908aa9dd8b48a5e75b219472
Yogesh Singh <yogesh@trimian.com>
2018-01-13 00:32:49 +0530

    Closure

---
+++ Closure/basic.js
@@ -0,0 +1,8 @@
+var num = 10;
+function sum(argument) {
+	return argument + num;
+}
+
+console.log(sum(5));
+num++;
+console.log(sum(5));
---
+++ Closure/hoisting.js
@@ -0,0 +1,11 @@
+function sum(argument) {
+	return argument + num;
+}
+
+console.log(sum(5));
+console.log(num);
+num++;
+console.log(num);
+console.log(sum(5));
+
+var num = 10;
